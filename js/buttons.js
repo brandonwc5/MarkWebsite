ec79fa6083c3d67d23948e9cb4cd5e7387f59aa5
brandonwc5 <brandonwc5@yahoo.com>
2018-06-09 16:50:53 -0500

    Fixes per request

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-    $("#actualBtn").click(function(){
-        window.open('mailto:markcharlesypc@gmail.com?subject=Reservation Request&body=Hi Mark! I would like to inquire about having my own YPC experience!');
-    })
-})
+$(document).ready(function() {
+  $(".submit_btn_bg").click(function() {
+    window.open("mailto:markcharlesypc@gmail.com?subject=Reservation Request&body=Hi Mark! I would like to inquire about having my own YPC experience!"
+    );
+  });
+  $(".headContact2").click(function() {
+    window.open("mailto:markcharlesypc@gmail.com?subject=Reservation Request&body=Hi Mark! I would like to inquire about having my own YPC experience!"
+    );
+  });
+  $("#submit").click(function(){
+      $(".emailto").append("<input type=\"email\" name=\"markcharlesypc@gmail.com\">");
+  });
+  //Not working
+//   $(".input-append date form_datetime").datetimepicker({
+//     onSelect: function() { 
+//         alert("doubleweiner");
+//         var dateObject = $(this).datepicker('getDate'); 
+//         $(".btn btn-default submit_btn").click(function(dateObject){
+//             $(".dinnerDate").append("<input type=\"text\" name=\"date\" value=(dateObject)>");
+//         });
+//     }
+//     });
+});
