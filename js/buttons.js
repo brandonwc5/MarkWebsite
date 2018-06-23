137935dda979e2f342a1950b52e4ac66245f0ec5
brandonwc5 <brandonwc5@yahoo.com>
2018-06-23 14:54:24 -0500

    checkpoint

--- js/buttons.js
+++ js/buttons.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     window.open("mailto:markcharlesypc@gmail.com?subject=Reservation Request&body=Hi Mark! I would like to inquire about having my own YPC experience!"
     );
   });
+  $(".submit_btn_bg2").click(function() {
+    window.open("mailto:markcharlesypc@gmail.com?subject=Reservation Request&body=Hi Mark! I would like to inquire about having my own YPC experience!"
+    );
+  });
   $(".headContact2").click(function() {
     window.open("mailto:markcharlesypc@gmail.com?subject=Reservation Request&body=Hi Mark! I would like to inquire about having my own YPC experience!"
     );
