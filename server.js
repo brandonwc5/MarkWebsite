8682a6833b5279589707227a9e412205094740a1
brandonwc5 <brandonwc5@yahoo.com>
2018-07-22 22:08:14 -0500

    serving on localhost

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT || 122;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Serve up static assets
-app.use(express.static("/"));
-
+//app.use(express.static("/"));
+app.use(express.static(__dirname + '/'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,11 +15,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Define any API routes before this runs
-app.get("/", function(req, res) {
-    console.log("Getting home path");
-  res.sendFile(path.join("/index.html"));
-});
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
 app.get("/about-us", function (req, res) {
     res.sendFile(path.join(__dirname, "./about-us.html"));
 });
